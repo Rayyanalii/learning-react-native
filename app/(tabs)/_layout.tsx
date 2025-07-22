182c06dd27a4369334ff20c671cc1c8a0d7d7252
Ayyan Ali <rayyanali2705@gmail.com>
2025-07-22 21:29:12 +0500

    Learned how to implement grouping of screens, navigation bar between them and styling it

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,99 @@
+import { icons } from "@/imports/icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageSourcePropType, Text, View } from "react-native";
+
+interface TabIconPropTypes {
+  focused: boolean;
+  icon: ImageSourcePropType;
+  title: string;
+}
+
+const TabIcon = ({ focused, icon, title }: TabIconPropTypes) => {
+  return (
+    <>
+      {focused ? (
+        <View
+          className={`flex-1 flex-row justify-center items-center bg-purple-300 w-full min-w-[112px] gap-2 rounded-full min-h-16 mt-4 overflow-hidden`}
+        >
+          <Image source={icon} tintColor={"#151312"} className="size-5" />
+          <Text className="text-secondary text-base font-semibold mt-1">
+            {title}
+          </Text>
+        </View>
+      ) : (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+          <Image source={icon} tintColor={"#A8B5DB"} className="size-5" />
+        </View>
+      )}
+    </>
+  );
+};
+
+const _Layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 15,
+          marginBottom: 30,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.saved} title="Saved" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.profile} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _Layout;
---
+++ imports/icons.ts
@@ -0,0 +1,6 @@
+export const icons = {
+  home: require("../assets/icons/home.png"),
+  search: require("../assets/icons/search.png"),
+  saved: require("../assets/icons/saved.png"),
+  profile: require("../assets/icons/profile.png"),
+};
